cdb3a7ac19487d4540fe8c9992dba58672c17886
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-02 13:11:36 +0300

    Конфигурация Babel и WebPack для ES модулей

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule('js')
+      .test(/\.js$/)
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap(options => ({
+        ...options,
+        sourceType: 'module',
+      }));
+
+    config.module
+      .rule('vue')
+      .test(/\.vue$/)
+      .use('vue-loader')
+      .loader('vue-loader');
+  },
+});

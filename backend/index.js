30ed8d845c254911d1a7e8c0f6359cd854ef4700
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-03 13:08:16 +0300

    Добавлен роут /api/login для пользовательской аутентификации

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,35 @@
-
 const express = require('express');
-const db = require('./config/db'); // Подключаем базу данных
-
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors') ;
 const app = express();
-
-// Разрешаем парсить JSON
+const port = 3000;
+// Подключаемся к SQLite
+const db = new sqlite3.Database('./database.db', (err) =>{
+if (err) {
+  console.error('Ошибка подключения к базе данных:', err.message);
+} else {
+  console.log('Подключено к базе данных SQLite.');
+}
+});
+app.use(cors());
 app.use(express.json());
 
-// Тестовый маршрут
 app.get('/', (req, res) => {
   res.json({ message: 'Сервер работает!' });
 });
-
+app.post('/api/login', (req,res) => {
+  const {email, password} = req.body;
+  db.get('SELECT * FROM users WHERE email = ? AND password = ?', [email,password], (err,row) => {
+if (err) {
+  res.status(500).json({message: 'Ошибка сервера' });
+} else if (row) {
+  res.json({message: 'Успешный вход', userId: row.id});
+} else {
+  res.status(401).json({message: 'Неверный email или пароль'});
+} 
+  });
+});
 // Запускаем сервер
-const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });

b58b3f9ac085494e30b86d216c53f33a0d1625f7
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-04 14:30:35 +0300

    Добавление роута /projects с проверкой аутентификации

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../App.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Profile from '../views/Profile.vue';
+import Projects from '../views/Projects.vue';
 const routes = [
   {
     path: '/',
@@ -22,7 +23,14 @@ const routes = [
   {
     path: '/profile',
     name: 'Profile',
-    component: Profile
+    component: Profile,
+    meta: { requiresAuth: true }
+  },
+  {
+    path:'/projects',
+    name:'Projects',
+    component: Projects,
+    meta: { requiresAuth: true }
   }
 ];
 
@@ -30,5 +38,16 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    try {
+      await axios.get('http://localhost:3000/api/profile', { withCredentials: true });
+      next();
+    } catch (error) {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
 export default router;

3384e8bd06392fbc111a07e1da1f172e293b8e1f
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-07 12:37:14 +0300

    Перепроверка импортируемых зависимостей

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css'; // CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // JS бандл с Modal и другими компонентами
 
 const app = createApp(App);
 app.use(router);
